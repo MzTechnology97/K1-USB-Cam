abc90c290962085b2e955ed1dd417238eece3483
MzTechnology97 <86824210+MzTechnology97@users.noreply.github.com>
2024-04-05 10:08:01 +0200

    Update dual_cam.sh

--- script/dual_cam.sh
+++ script/dual_cam.sh
@@ -6,4 +6,4 @@ kill -9 $(fuser /dev/video5)
 kill -9 $(fuser /dev/video6)
 
 
-/opt/bin/mjpg_streamer -b -i "/opt/lib/mjpg-streamer/input_uvc.so -d /dev/video5  -q 50  -r 1920x1080 -f 30" -i "/opt/lib/mjpg-streamer/input_uvc.so -d /dev/video4" -o "/opt/lib/mjpg-streamer/output_http.so -p 8080"
+/opt/bin/mjpg_streamer -b -i "/opt/lib/mjpg-streamer/input_uvc.so -d /dev/video5  -q 40  -r 1080x720 -f 25" -i "/opt/lib/mjpg-streamer/input_uvc.so -d /dev/video4 -q 40  -r 1080x720 -f 25" -o "/opt/lib/mjpg-streamer/output_http.so -p 8080"
