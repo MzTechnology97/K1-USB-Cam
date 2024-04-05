b96b8666586ef6bced705c88bca702802fbdf50b
MzTechnology97 <86824210+MzTechnology97@users.noreply.github.com>
2024-04-05 10:03:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-Single_USB_Camera()
-{
-    echo "\n$msg \nInstalling Single USB camera...\n"
+install_packages() {
+    echo -e "\n$msg\nInstalling required packages...\n"
+    opkg install mjpg-streamer mjpg-streamer-input-uvc mjpg-streamer-output-http
+    echo "Packages installed successfully."
+}
+
+Single_USB_Camera() {
+    echo -e "\n$msg\nInstalling Single USB camera...\n"
     cp -f script/single_cam.sh /usr/data/startup_usb_camera.sh
     chmod +x /usr/data/startup_usb_camera.sh
 
@@ -12,15 +17,13 @@ Single_USB_Camera()
     echo "Successfully Installed USB Camera"
 
     echo "Starting Webcam Service"
-    chmod +x /etc/init.d/S99usb_camera
     /etc/init.d/S99usb_camera start
 
-    echo  "\n \e[1;31m Please consider reboot your printer to make sure that all process will work property.\e[0m \n" 
+    echo -e "\n \e[1;31m Please consider reboot your printer to make sure that all processes will work properly.\e[0m \n" 
 }
 
-Dual_USB_Camera()
-{
-    echo "\n$msg \nInstalling Dual USB camera...\n"
+Dual_USB_Camera() {
+    echo -e "\n$msg\nInstalling Dual USB camera...\n"
     cp -f script/dual_cam.sh /usr/data/startup_usb_camera.sh
     chmod +x /usr/data/startup_usb_camera.sh
 
@@ -30,43 +33,47 @@ Dual_USB_Camera()
     echo "Successfully Installed K1 + USB Camera"
 
     echo "Starting Webcam Service"
-    chmod +x /etc/init.d/S99usb_camera
     /etc/init.d/S99usb_camera start
 
-    echo "\n \e[1;31m Please consider reboot your printer to make sure that all process will work property.\e[0m \n" 
+    echo -e "\n \e[1;31m Please consider reboot your printer to make sure that all processes will work properly.\e[0m \n" 
 }
-
 
 echo "" 
 PS=
 
 while : 
 do
-
-    
-
     echo "1) Install Single USB Camera"
-
     echo "2) Install K1 + USB Cam"
+    echo "3) Install required packages"
+    echo "4) exit"
 
-    echo "3) exit"
-
-    echo "\nHow can I help you today? "
+    echo -e "\nHow can I help you today? "
 
     read msg;
 
     case $msg in
-        1) echo "Install Single USB Camera"
+        1)
+            echo "Install Single USB Camera"
             Single_USB_Camera
-            exit ;;
-        2) echo "Install K1 + USB Cam"
-           Dual_USB_Camera 
-           exit ;;
-        3) echo "Quit"
-           echo "We're done here."
-           exit ;;
+            exit 
+            ;;
+        2)
+            echo "Install K1 + USB Cam"
+            Dual_USB_Camera 
+            exit 
+            ;;
+        3)
+            install_packages
+            ;;
+        4)
+            echo "Quit"
+            echo "We're done here."
+            exit 
+            ;;
         *)
-           echo "\nOoops, invalid option.\n"
+            echo -e "\nOoops, invalid option.\n"
+            ;;
     esac
-    sleep .5
+    sleep 0.5
 done
